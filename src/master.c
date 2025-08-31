a9e4b6ef2dab61ce11c03dbab15d9e5941537b66
cyelmini <cyelmini@itba.edu.ar>
2025-08-31 17:36:48 -0300

    Comienzo master
    Co-Authored-By: Olivia Rodrigue <138537751+OliviaRodrigue@users.noreply.github.com>
    Co-Authored-By: Mateo Lopez Badias <152566466+mlopezbadias@users.noreply.github.com>

---
+++ src/master.c
@@ -0,0 +1,10 @@
+#include "include/utils.h"
+
+int main (int argc, char *argv[]){
+    if(argc<3){
+        fprintf(stderr, "ERROR: No se ha especificado jugador");
+        exit(EXIT_FAILURE);
+    }
+    
+    
+}
---
+++ src/mlib.c
@@ -0,0 +1,3 @@
+#include "include/mlib.h"
+
+//void handle_params(int argc, char *argv[], int width, int *height, int *delay,  )
