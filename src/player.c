47bf5924a788feb0ebbf771a62f00c9298df4b5c
cyelmini <cyelmini@itba.edu.ar>
2025-08-31 16:53:01 -0300

    player
    Co-Authored-By: Olivia Rodrigue <138537751+OliviaRodrigue@users.noreply.github.com>
    Co-Authored-By: Mateo Lopez Badias <152566466+mlopezbadias@users.noreply.github.com>

--- src/player.c
+++ src/player.c
@@ -1,117 +1,134 @@
 #include "include/utils.h"
 #include <stdlib.h>
+#include <time.h>
+#include <sched.h>
 
-int dir[8][2] = {{0, 1}, {0, -1}, {1, 0}, {1, 1}, {1, -1}, {-1, 0}, {-1, -1}, {-1, 1}};
+static const int DIRS[8][2] = {
+    { 0,-1}, // 0 ARRIBA
+    { 1,-1}, // 1 ARRIBA A LA DERECHA
+    { 1, 0}, // 2 DERECHA
+    { 1, 1}, // 3 ABAJO A LA DERECHA
+    { 0, 1}, // 4 ABAJO
+    {-1, 1}, // 5 ABAJO A LA IZQUIERDA
+    {-1, 0}, // 6 IZQUIERDA
+    {-1,-1}, // 7 ARRIBA A LA IZQUIERDA
+};
 
-static int is_valid(game_t *game, int x, int y) {
-	if (x < 0 || x >= game->width || y < 0 || y >= game->height)
+static int in_bounds(const game_t *game, int x, int y) {
+    return (x >= 0 && x < game->width && y >= 0 && y < game->height);
+}
+ 
+static int is_valid_cell(const game_t *game, int x, int y) {
+    if (!in_bounds(game, x, y)){
 		return 0;
-	for (unsigned int i = 0; i < game->playerCount; ++i) {
-		if (game->players[i].x == x && game->players[i].y == y && !game->players[i].isBlocked)
-			return 0;
+	} 
+
+    if (game->board[y * game->width + x] <= 0){
+		return 0;
 	}
-	return 1;
+
+    for (unsigned int i = 0; i < game->playerCount; ++i) {
+        const player_t * player = &game->players[i];
+        if (!player->isBlocked && player->x == x && player->y == y) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
-static int simulate_moves(game_t *game, int x, int y, int depth, int *board, int width, int height) {
-	if (depth == 0)
-		return 0;
-	int max_score = 0;
-	for (int d = 0; d < 8; ++d) {
-		int nx = x + dir[d][0];
-		int ny = y + dir[d][1];
-		if (!is_valid(game, nx, ny))
-			continue;
-		int cell_score = board[ny * width + nx];
-		int path_score = cell_score + simulate_moves(game, nx, ny, depth - 1, board, width, height);
-		if (path_score > max_score)
-			max_score = path_score;
-	}
-	if (max_score == 0)
-		return 0;
-	return max_score;
-}
+static int choose_move(const game_t *game, const player_t *me) {
+    int best_dir = -1;
+    int best_score = -1; 
 
-static int pathfinder(game_t *game, player_t *player) {
-	int best_dir = -1;
-	int best_score = 0;
-	int width = game->width;
-	int height = game->height;
-	int *board = game->board;
-
-	for (int d = 0; d < 8; ++d) {
-		int nx = player->x + dir[d][0];
-		int ny = player->y + dir[d][1];
-		if (!is_valid(game, nx, ny))
-			continue;
-		int cell_score = board[ny * width + nx];
-		int path_score = cell_score + simulate_moves(game, nx, ny, 2, board, width, height);
-		if (path_score > best_score) {
-			best_score = path_score;
-			best_dir = d;
+    for (int dir = 0; dir < 8; ++dir) {
+        int nx = me->x + DIRS[dir][0];
+        int ny = me->y + DIRS[dir][1];
+       
+		if(is_valid_cell(game, nx, ny)){
+			int score = game->board[ny * game->width + nx];
+			if (score > best_score) {
+				best_score = score;
+				best_dir = dir;
+			}
 		}
-	}
-	return best_dir;
+    }
+    return best_dir; 
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 1) {
-		fprintf(stderr, "Uso: %s\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s <width> <height>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-	// Mapear memoria compartida
-	game_t *game = (game_t *) open_and_map(SHM_GAME, O_RDWR, 0, sizeof(game_t) + sizeof(int) * 100 * 100,
-										   PROT_READ | PROT_WRITE, MAP_SHARED);
-	game_sync *sync =
-		(game_sync *) open_and_map(SHM_SYNC, O_RDWR, 0, sizeof(game_sync), PROT_READ | PROT_WRITE, MAP_SHARED);
+    int width  = atoi(argv[1]);
+    int height = atoi(argv[2]);
 
-	pid_t pid = getpid();
-	int player_number = -1;
-	for (unsigned int i = 0; i < game->playerCount; i++) {
-		if (game->players[i].player_pid == pid) {
-			player_number = i;
+    // Mapear memoria compartida 
+    game_t *game = (game_t *)open_and_map(SHM_GAME, O_RDONLY, sizeof(game_t) + sizeof(int) * width * height, PROT_READ, MAP_SHARED);
+    game_sync *sync = (game_sync *)open_and_map(SHM_SYNC, O_RDWR, sizeof(game_sync), PROT_READ | PROT_WRITE, MAP_SHARED);
+
+    // Identificar pid
+    pid_t me_pid = getpid();
+    int my_id = -1;
+    for (unsigned int i = 0; i < game->playerCount; ++i) {
+        if (game->players[i].player_pid == me_pid) { 
+			my_id = (int)i; 
+			break; 
+		}
+    }
+    if (my_id < 0) {
+        fprintf(stderr, "Jugador no encontrado en game->players (pid=%d)\n", me_pid);
+        exit(EXIT_FAILURE);
+    }
+
+    while (!game->gameFinished && !game->players[my_id].isBlocked) {
+        // Esperar turno
+        sem_wait(&sync->playerTurn[my_id]);
+
+        // Puede haber terminado mientras esperaba turno
+        if (game->gameFinished || game->players[my_id].isBlocked) {
 			break;
 		}
-	}
-	if (player_number == -1) {
-		fprintf(stderr, "Jugador no encontrado.\n");
-		exit(EXIT_FAILURE);
-	}
 
-	player_t *player = &game->players[player_number];
+        // Ver si el master quiere escribir
+        sem_wait(&sync->masterMutex);
+        sem_post(&sync->masterMutex);
 
-	while (!player->isBlocked && !game->gameFinished) {
-		// Ver si puede pasar a leer
-		sem_wait(&sync->masterMutex);
-		sem_post(&sync->masterMutex);
-
-		// Lectura segura del estado del juego
-		sem_wait(&sync->readersMutex);
-		sync->readersCount++;
-		if (sync->readersCount == 1) {
+        // Entrar como lector
+        sem_wait(&sync->readersMutex);
+        sync->readersCount++;
+        if (sync->readersCount == 1) {
 			sem_wait(&sync->gameMutex);
 		}
-		sem_post(&sync->readersMutex);
+        sem_post(&sync->readersMutex);
 
-		unsigned char move = pathfinder(game, player);
+        player_t *me = &game->players[my_id];
+        int dir = choose_move(game, me);
 
-		// Fin de lectura
-		sem_wait(&sync->readersMutex);
-		sync->readersCount--;
-		if (sync->readersCount == 0)
+        // Salir como lector
+        sem_wait(&sync->readersMutex);
+        sync->readersCount--;
+        if (sync->readersCount == 0) {
 			sem_post(&sync->gameMutex);
-		sem_post(&sync->readersMutex);
-
-		// Envia el movimiento al master por stdout
-		if (write(STDOUT_FILENO, &move, sizeof(unsigned char)) == -1) {
-			perror("write");
-			exit(EXIT_FAILURE);
 		}
-	}
+        sem_post(&sync->readersMutex);
 
-	close_and_unmap(SHM_GAME, (void *) game, sizeof(game_t) + sizeof(int) * 100 * 100, false);
-	close_and_unmap(SHM_SYNC, (void *) sync, sizeof(game_sync), false);
+        // Si no hay movimiento válido, termino
+        if (dir < 0) {
+			break;
+		}
 
-	return 0;
+        // Enviar movimiento al master 
+        unsigned char move = (unsigned char)dir;
+        if (write(STDOUT_FILENO, &move, sizeof(move)) == -1) {
+            perror("write");
+            break;
+        }
+    }
+
+    close_and_unmap(SHM_GAME, game, sizeof(game_t) + sizeof(int) * width * height, false);
+    close_and_unmap(SHM_SYNC, sync, sizeof(game_sync), false);
+
+    return 0;
 }
